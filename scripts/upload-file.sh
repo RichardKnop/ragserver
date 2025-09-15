940c8d8534aba989f7272f358816e45eea16b77f
Richard Knop <richard.knop@esgbook.com>
2025-09-15 16:12:54 +0100

    chore: refactor upload file script to poll until uploaded file is processed

--- scripts/upload-file.sh
+++ scripts/upload-file.sh
@@ -8,14 +8,31 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-set -x
-
-# Capture the file path from the command-line argument
+# Upload a file to the ragserver and capture the uploaded file ID
 FILE=$1
 
-# Upload a file to the ragserver
-curl \
-    -X POST \
+file_id=$(curl -X POST \
     -H 'Content-Type: multipart/form-data' \
     -F file=@"$FILE" \
-    http://localhost:8080/files | jq .
+    http://localhost:8080/files -s | jq -r ".id");
+
+printf "\nUploading a file with ID $file_id\n"
+
+file_url="http://localhost:8080/files/$file_id"
+interval_in_seconds=1
+status_path=".status"
+
+printf "\nPolling '${file_url%\?*}' every $interval_in_seconds seconds, until processing successful or failed\n"
+
+while true;
+do
+    status=$(curl -H 'Content-Type: application/json' $file_url | jq -r $status_path);
+    printf "\r$(date +%H:%M:%S): $status";
+    if [[ "$status" == "PROCESSED_SUCCESSFULLY" || "$status" == "PROCESSING_FAILED" ]]; then
+        curl \
+            -H 'Content-Type: application/json' \
+            ${file_url} | jq .
+        break;
+    fi;
+    sleep $interval_in_seconds;
+done
